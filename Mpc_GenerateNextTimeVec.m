741b2a18506d1009053c542b293cc83c09113146
georgejajji <64091746+georgejajji@users.noreply.github.com>
2020-04-21 18:39:59 +0200

    Update Mpc_GenerateNextTimeVec.m

--- Mpc_GenerateNextTimeVec.m
+++ Mpc_GenerateNextTimeVec.m
@@ -1,3 +1,4 @@
+% Created by Robin Holmbom Ph.D. Student at Linkoping University
 % Generates the upcoming time vector in the MPC. 
 % timeVec = [t0, t0+Ts, t0+2*Ts,...,t0+(N-1)*Ts]
 % Mainly used for reference generation.
