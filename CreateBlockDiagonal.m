5c632c5806afed51ea824f8dc4c2c1437efdfd84
georgejajji <64091746+georgejajji@users.noreply.github.com>
2020-04-21 18:40:12 +0200

    Update CreateBlockDiagonal.m

--- CreateBlockDiagonal.m
+++ CreateBlockDiagonal.m
@@ -1,4 +1,4 @@
-% Created by Robin Holmbom Ph.D. Student
+% Created by Robin Holmbom Ph.D. Student at Linkoping University
 % Creates a block diagonal matrix of A with dimension n.
 % A: k x m
 % B: k*n x m*n
